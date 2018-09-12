481d2ffd63f426e407c95ac85009a0f6d016dde7
Sun Zhenyuan <sunzhenyuan@baidu.com>
2018-09-11 23:58:23 -0400

    add mkinitramfs helper scripts
    Signed-off-by: Sun Zhenyuan <sunzhenyuan@baidu.com>

---
+++ mlnx-nvme-4.0/nvmf-mod/mkinitramfs.sh
@@ -0,0 +1,34 @@
+is6u3=0
+grep 6.3 /etc/redhat-release > /dev/null
+if [ $? -eq 0 ]; then
+    is6u3=1
+fi
+
+mkdir ./tmp
+t=`date +"%Y%m%d%H%M%S"`
+
+if [ $is6u3 -eq 1 ]; then
+    installkernel_conf=/usr/share/dracut/modules.d/90kernel-modules/installkernel
+    cp $installkernel_conf ./tmp/installkernel_$t
+    sed /scsi_wait_scan/d ./tmp/installkernel_$t > ./tmp/installkernel.new
+    cp ./tmp/installkernel.new $installkernel_conf
+
+    init_sh=/usr/share/dracut/modules.d/99base/init
+    cp $init_sh ./tmp/init_$t
+    sed /scsi_wait_scan/d ./tmp/init_$t > ./tmp/init.new
+    cp ./tmp/init.new $init_sh
+fi
+
+K_VER=`uname -r`
+/sbin/dracut --force --add-drivers "nvme" --omit-drivers "mlx5_core mlx5_ib mlx_compat" /boot/initrd-${K_VER}.img ${K_VER}
+rm -rf /boot/initrd-${K_VER}kdump.img
+
+sync
+echo 3 > /proc/sys/vm/drop_caches
+sync
+
+cp -f /boot/initrd-${K_VER}.img ./tmp/i.gz
+cd ./tmp/
+gunzip i.gz
+cpio -id < i
+find -name "nvme*.ko"
